d8eb7317136d5734a219f095bfa041f1853aff4b
Taras Karpuk <taras.karpuk@gmail.com>
2021-12-03 17:13:38 +0200

    Add AppDbContext

---
+++ Beetroot.DAL/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Beetroot.DAL.Configurations;
+using Beetroot.DAL.Entities;
+using Beetroot.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beetroot.DAL
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AddressConfiguration());
+            modelBuilder.ApplyConfiguration(new MessageConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Beetroot.DAL/Interfaces/IAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Beetroot.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Beetroot.DAL.Interfaces
+{
+    public interface IAppDbContext
+    {
+        DbSet<Address> Addresses { get; set; }
+        DbSet<Message> Messages { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
